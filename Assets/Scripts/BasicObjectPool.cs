882d4a7100c8c221b209e8ee730b50f29b87820e
Fox <jcfox@labs.neit.edu>
2018-03-07 13:00:58 -0500

    starting point

---
+++ Assets/Scripts/BasicObjectPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicObjectPool : MonoBehaviour {
+	public GameObject prefab;
+	private Stack<GameObject> inactiveInstances = new Stack<GameObject> ();
+
+	public GameObject GetObject () {
+		GameObject spawnedObject;
+
+		if (inactiveInstances.Count > 0) {
+			spawnedObject = inactiveInstances.Pop ();
+		} else {
+			spawnedObject = Instantiate (prefab);
+
+//			PooledObject pooledObject = spawnedObject.AddComponent<PooledObject> ();
+//			pooledObject.pool = this;
+
+			spawnedObject.AddComponent<PooledObject> ().pool = this;
+		}
+
+		spawnedObject.SetActive (true);
+		return spawnedObject;
+	}
+
+	public void ReturnObject (GameObject toReturn) {
+		PooledObject pooledObject = toReturn.GetComponent<PooledObject> ();
+
+		if (pooledObject != null && pooledObject.pool == this) {
+			toReturn.SetActive (false);
+			inactiveInstances.Push (toReturn);
+		} else {
+			Destroy (toReturn);
+		}
+	}
+
+//	void Start () {
+//		
+//	}
+//
+//	void Update () {
+//		
+//	}
+}
+
+public class PooledObject : MonoBehaviour {
+	public BasicObjectPool pool;
+}
---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour {
+	public RoundData[] allRoundData;
+	public int currentRound = 0;
+
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+		SceneManager.LoadScene ("MenuScreen");
+	}
+
+	public RoundData GetCurrentRoundData () {
+		return allRoundData[currentRound];
+	}
+}
